fd98d454c4512635c8ea72e69f1a8d7d4e63a2a1
iwa-ma <iwakura31@gmail.com>
2025-07-20 21:02:06 +0900

    [共通] add : アプリタイトル変更

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FitnessQuest',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'FitnessQuest'),
     );
   }
 }
@@ -34,6 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('FitnessQuest'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
